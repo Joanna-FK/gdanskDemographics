f4ef262fd15a811690e611ce6ce7d8e13705315e
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-01-16 22:30:06 +0100

    District class is finished

--- src/main/java/pl/sda/gdanskdemographics/District.java
+++ src/main/java/pl/sda/gdanskdemographics/District.java
@@ -21,4 +21,40 @@ public class District {
         this.ageFromSixteenToSixtyFour = ageFromSixteenToSixtyFour;
         this.ageOverSixtyFour = ageOverSixtyFour;
     }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getAgeUnderEighteen() {
+        return ageUnderEighteen;
+    }
+
+    public Integer getAgeFromEighteenToFiftyNine() {
+        return ageFromEighteenToFiftyNine;
+    }
+
+    public Integer getAgeFromSixteenToSixtyFour() {
+        return ageFromSixteenToSixtyFour;
+    }
+
+    public Integer getAgeOverSixtyFour() {
+        return ageOverSixtyFour;
+    }
+
+    @Override
+    public String toString() {
+        return "District{" +
+                "districtName='" + districtName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", ageUnderEighteen=" + ageUnderEighteen +
+                ", ageFromEighteenToFiftyNine=" + ageFromEighteenToFiftyNine +
+                ", ageFromSixteenToSixtyFour=" + ageFromSixteenToSixtyFour +
+                ", ageOverSixtyFour=" + ageOverSixtyFour +
+                '}';
+    }
 }
