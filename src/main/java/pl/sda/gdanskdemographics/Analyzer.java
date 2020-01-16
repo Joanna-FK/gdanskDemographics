614775d88099285da2433084690b8803ba070af8
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-01-16 22:38:21 +0100

    exercise is done...

--- src/main/java/pl/sda/gdanskdemographics/Analyzer.java
+++ src/main/java/pl/sda/gdanskdemographics/Analyzer.java
@@ -2,4 +2,42 @@ package pl.sda.gdanskdemographics;
 
 public class Analyzer {
 
+    private District[] allDistricts;
+
+    public Analyzer(District[] allDistricts) {
+        this.allDistricts = allDistricts;
+    }
+
+    public void findByName(String districtName) {
+        for (District singleDistrict : allDistricts) {
+            if (singleDistrict.getDistrictName().contains(districtName)) {
+                System.out.println(singleDistrict);
+            }
+        }
+    }
+
+    public District nameWithLargestResidents(String gender, Integer age) {
+        District singleDistrictName = allDistricts[0];
+
+        for (int i = 1; i < allDistricts.length-2; i++) {
+            if (gender.equals("kobiety") || gender.equals("mężczyźni") &&
+                   singleDistrictName.getAgeUnderEighteen() < allDistricts[i].getAgeUnderEighteen() ||
+                    singleDistrictName.getAgeFromEighteenToFiftyNine()< allDistricts[i].getAgeFromEighteenToFiftyNine() ||
+                    singleDistrictName.getAgeFromSixteenToSixtyFour() < allDistricts[i].getAgeFromSixteenToSixtyFour() ||
+                    singleDistrictName.getAgeOverSixtyFour() < allDistricts[i].getAgeOverSixtyFour())
+                     {
+                         singleDistrictName = allDistricts[i];
+                     }
+        }
+        return singleDistrictName;
+    }
+
+    public void mostResidentOfGender() {
+        for (District allDistrict : allDistricts) {
+           int  genderSum = allDistrict.getAgeOverSixtyFour() + allDistrict.getAgeFromSixteenToSixtyFour() + allDistrict.getAgeUnderEighteen() + allDistrict.getAgeFromEighteenToFiftyNine();
+            System.out.println(allDistrict.getDistrictName() + " " + allDistrict.getGender() + " " + genderSum);
+        }
+    }
 }
+
+
--- src/main/java/pl/sda/gdanskdemographics/App.java
+++ src/main/java/pl/sda/gdanskdemographics/App.java
@@ -1,30 +1,73 @@
 package pl.sda.gdanskdemographics;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        District [] districts = loadDistricts();
-        }
+    public static void main(String[] args) throws Exception {
+        District[] districts = loadDistricts();
+        Analyzer analyzer = new Analyzer(districts);
+        analyzer.findByName("ZASPA");
+
+        Integer ageUnderEighteen = null;
+        Integer ageFromEighteenToFiftyNine = null;
+        Integer ageFromSixteenToSixtyFour = null;
+        Integer ageOverSixtyFour = null;
+        District singleDistrictName = analyzer.nameWithLargestResidents("mężczyźni", ageFromEighteenToFiftyNine);
+        System.out.println(singleDistrictName.getDistrictName());
+
+        analyzer.mostResidentOfGender();
+        System.out.println();
+
+
+
+    }
 
     private static District[] loadDistricts() throws IOException {
         String loadedData = FileLoaderUtil.readFileAsSingleStringByFilesReadString("data.csv");
         String[] rows = loadedData.split("\r\n");
 
-        District [] districts = new District[rows.length-1];
-        for(int i=1; i<rows.length; i++){
+        District[] districts = new District[rows.length - 1];
+        for (int i = 1; i < rows.length; i++) {
             String[] splitRow = rows[i].split(";");
 
             District singleDistrict = new District(
                     splitRow[0],
-                    splitRow [1],
-                    Integer.valueOf(splitRow[2].replaceAll(" ","")),
-                    Integer.valueOf(splitRow[3].replaceAll(" ","")),
-                    Integer.valueOf(splitRow[4].replaceAll(" ","")),
-                    Integer.valueOf(splitRow[5].replaceAll(" ",""))
-            );
-            districts [i-1] = singleDistrict;
+                    splitRow[1],
+                    Integer.valueOf(splitRow[2].replaceAll(" ", "")),
+                    Integer.valueOf(splitRow[3].replaceAll(" ", "")),
+                    Integer.valueOf(splitRow[4].replaceAll(" ", "")),
+                    Integer.valueOf(splitRow[5].replaceAll(" ", "")));
 
-        }return districts;
+            districts[i - 1] = singleDistrict;
+        }
+        return districts;
     }
+
+/*static void readData(){
+      Scanner scanner =  new Scanner(System.in);
+      menu();
+         int number = scanner.nextInt();
+        switch (number) {
+            case 1: opcjaPierwsza();
+            break;
+            case 2:opcjaDruga();
+            break;
+            case 3: opcjaTrzecia();
+
+        }
+    }
+    private static void opcjaDruga() {
+        System.out.println("Wyszukiwanie danych po kryterium wiekowym ");
+    }
+    private static void opcjaPierwsza() {
+        System.out.println("Wyszukiwanie danych po nazwie dzielnicy");
+    }
+    private static void opcjaTrzecia() {
+        System.out.println("Wyszukiwanie danych po nazwie płci");
+    }
+    private static void menu (){
+        System.out.println("Wybierz opcję wyszukiwania danych: \n 1. Kryterium wiekowe; \n 2. Nazwa dzielnicy; \n 3. Płeć; \n  ");
+    }*/
+
 }
